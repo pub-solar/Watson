ce7518e9ee5e341dfc92216e157aa5d851661062
b12f <git@benjaminbaedorf.eu>
2024-11-25 12:12:29 +0100

    flake: improve devshell, add watson package, add github action

--- flake.nix
+++ flake.nix
@@ -1,51 +1,77 @@
 {
   description = "Watson devenv";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  inputs.devshell.url = "github:numtide/devshell";
-  inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
 
-  outputs = {self, nixpkgs, systems, devshell }:
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import systems);
-      # Nixpkgs instantiated for system types in nix-systems
-      nixpkgsFor = eachSystem (system:
-        import nixpkgs {
-          inherit system;
-          overlays = [
-            devshell.overlays.default
-          ];
-        }
-      );
-    in
-    {
-      devShells = eachSystem (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.devshell.mkShell {
-            # Add additional packages you'd like to be available in your devshell
-            # PATH here
-            devshell.packages = with pkgs; [
+  outputs = inputs@{ self, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        args@{ pkgs, config, system, ... }: {
+          packages = {
+            watson-td = pkgs.python3Packages.buildPythonPackage {
+              name = "watson-td";
+              src = ./.;
+              propagatedBuildInputs = with pkgs.python3Packages; [
+                # requirements
+                click
+                click-didyoumean
+                requests
+                arrow
+
+                # requirements-dev
+                flake8
+                py
+                pytest
+                pytest-datafiles
+                pytest-mock
+                pytest-runner
+                pytest-runner
+              ];
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
               gnumake
               virtualenv
-              (pkgs.python3.withPackages (p: [
-                # select Python packages here
+              yaml-language-server
+              (python3.withPackages (p: [
+                # LSP support
                 p.python-lsp-ruff
                 p.python-lsp-server
                 p.python-lsp-server.optional-dependencies
+
+                # pip
+                p.pip
+
+                # requirements
+                p.click
+                p.click-didyoumean
+                p.requests
+                p.arrow
+
+                # requirements-dev
+                p.flake8
+                p.py
+                p.pytest
+                p.pytest-datafiles
+                p.pytest-mock
+                p.pytest-runner
+                p.pytest-runner
               ]))
             ];
           };
-        });
-
-      packages = eachSystem (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-        });
+        };
     };
 }
